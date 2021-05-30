ed6dbb1c3e3b04a1a85340d7bb6ec773ca9de344
Aman-14 <akumar999022@gmail.com>
2021-05-30 17:01:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const findTeam = (content) => {
+  content = new RegExp(/team.*/i).exec(content);
+  if (!content) {
+    return "NOT FOUND";
+  }
+
+  const foundTeam = content[0]
+    .replace(/team|name|<@*#*!*&*\d+>|[^\w\s]/gi, "")
+    .trim();
+
+  return foundTeam ? `Team ${foundTeam}` : "NOT FOUND";
+};
+
+app.post("/slotlist", (req, res) => {
+  const slotlist = [];
+  if (!req.body?.content_array || !Array.isArray(req.body?.content_array)) {
+    return res.status(404).send({ message: "Empty Body" });
+  }
+  req.body.content_array.forEach((content) => {
+    slotlist.push(findTeam(content));
+  });
+
+  res.json(slotlist);
+});
+
+app.listen(3500, () => {
+  "Server is running";
+});
